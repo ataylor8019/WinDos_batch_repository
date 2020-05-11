900f791a5fe78d62c94e9243e9629a62ff5f561b
ataylor8019 <39507029+ataylor8019@users.noreply.github.com>
2020-05-11 18:57:31 -0400

    Updated Sync process to run setup script
    Updated Sync process to run setup stored procedure along with table fill stored procedures

--- PizzaMakerSyncProcess.bat
+++ PizzaMakerSyncProcess.bat
@@ -3,6 +3,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET IMPORTFILE=""
 
+sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d TestPizzaImport -Q "EXEC SetupPizzaInformationStore"
 sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -i .\PizzaMakerFileFlattenRawTextFileLoad.sql
 sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d TestPizzaImport -Q "EXEC CUSTOMERLOAD"
 sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d TestPizzaImport -Q "EXEC MENULOAD"
