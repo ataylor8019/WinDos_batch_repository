f1437ec29271e4ffc8d7012202e2c5c9521c26c2
ataylor8019 <39507029+ataylor8019@users.noreply.github.com>
2020-05-14 21:42:38 -0400

    Driver update
    Updated batch file to run stored procedures, accept file location argument for BULK INSERT procedure.

--- PizzaMakerSyncProcess.bat
+++ PizzaMakerSyncProcess.bat
@@ -1,11 +1,9 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET IMPORTFILE=""
-
-sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d TestPizzaImport -Q "EXEC SetupPizzaInformationStore"
-sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -i .\PizzaMakerFileFlattenRawTextFileLoad.sql
-sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d TestPizzaImport -Q "EXEC CUSTOMERLOAD"
-sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d TestPizzaImport -Q "EXEC MENULOAD"
-sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d TestPizzaImport -Q "EXEC ORDERSLOAD"
-sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d TestPizzaImport -Q "EXEC ORDEREDITEMSLOAD"
+sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d PIZZALOGISTICS -Q "EXEC SetupPizzaInformationStore"
+sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d PIZZALOGISTICS -Q "EXEC BULKPIZZADATA_RAWIMPORT @FILENAME=$(IMPORTFILE)" -v IMPORTFILE=""
+sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d PIZZALOGISTICS -Q "EXEC CUSTOMERLOAD"
+sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d PIZZALOGISTICS -Q "EXEC MENULOAD"
+sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d PIZZALOGISTICS -Q "EXEC ORDERSLOAD"
+sqlcmd -Snp:\\.\pipe\MSSQL$SQLEXPRESS\sql\query -d PIZZALOGISTICS -Q "EXEC ORDEREDITEMSLOAD"
